1ea3e2626e7de4fef612a69da65af9e400b8279f
dchen278 <dchen30@stuy.edu>
2021-09-30 13:15:27 -0400

    08_hellofxn

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+	public static void main(String[] args) {
+		greet("Jason");
+		greet("Jaylen");
+		greet("Mr. Mykolyk");
+	}
+
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name +". How do you do?");
+	}
+}
