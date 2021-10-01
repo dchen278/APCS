659fd339bcde9b211db93244d461cdbca8073b51
dchen278 <dchen30@stuy.edu>
2021-10-01 13:16:09 -0400

    hw 09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,19 @@
+package BigSib;
+
+public class BigSib {
+	/*
+	DISCOVERIES
+	A method signature consists the method name and the method parameters in order
+	UNRESOLVED QUESTIONS
+	No questions were unresolved
+	*/
+	public static void main(String[] args) {
+		greet("Jason");
+		greet("Jaylen");
+		greet("Mr. Mykolyk");
+	}
+
+	public static void greet(String name) {
+		System.out.println("What's up " + name +". How is your day?");
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,16 @@
+package BigSib;
+
+public class Greet {
+	/*
+	DISCOVERIES
+	A method signature consists the method name and the method parameters in order
+	UNRESOLVED QUESTIONS
+	No questions were unresolved
+	*/
+	
+	public static void main(String[] args) {
+		BigSib.greet("Jason");
+		BigSib.greet("Jaylen");
+		BigSib.greet("Mr. Mykolyk");
+	}
+}
