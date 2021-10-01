beaf91c1abb3c87ea79493dab963462f4a9f5eab
dchen278 <dchen30@stuy.edu>
2021-10-01 13:19:41 -0400

    ok

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -7,11 +7,6 @@ public class BigSib {
 	UNRESOLVED QUESTIONS
 	No questions were unresolved
 	*/
-	public static void main(String[] args) {
-		greet("Jason");
-		greet("Jaylen");
-		greet("Mr. Mykolyk");
-	}
 
 	public static void greet(String name) {
 		System.out.println("What's up " + name +". How is your day?");
