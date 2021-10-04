da0522920866af8174be19c6284fdf1c31f1d1fe
dchen278 <reindeer914@gmail.com>
2021-10-04 19:49:58 -0400

    10_bigsib

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,18 @@
+/*
+BDY: David Chen, Yat Long Chan, Ben Belotser, Eugene, Bob, TwoFace
+David Chen
+APCS
+HW010 -- Refactor Big Sib One
+2021-10-04
+
+DISCOVERIES
+Returntype dictates the type returned by the method
+UNRESOLVED QUESTIONS
+No questions were unresolved
+*/
+
+public class BigSib {
+	public static String greet(String name) {
+		return "What's up " + name +". How is your day?";
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/*
+BDY: David Chen, Yat Long Chan, Ben Belotser, Eugene, Bob, TwoFace
+David Chen
+APCS
+HW010 -- Refactor Big Sib One
+2021-10-04
+
+DISCOVERIES
+Returntype dictates the type returned by the method
+UNRESOLVED QUESTIONS
+No questions were unresolved
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		System.out.println( BigSib.greet("Yat Long") );
+		System.out.println( BigSib.greet("Jaylen") );
+		System.out.println( BigSib.greet("Mr. Mykolyk") );
+	}
+}
