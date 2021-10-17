ef63d850efb842a0b152b0dbdb971297d82ced9a
dchen278 <reindeer914@gmail.com>
2021-10-17 18:59:33 -0400

    18

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,9 +1,23 @@
+/*
+Downtown Leopard Rocks: David Chen, Lea Kwok, Raven Tang
+APCS
+HW18: CPA-One
+2021-10-16
+
+DISCO
+UML diagrams are very helpful for understanding and creating methods.
+QCC
+None
+
+Downtown Leopard Rocks 
+*/
+
 public class BankAccount {
 	private String holderName;
 	private static String password;
 	private int pin;
 	private int accountNum;
-	private float balance;
+	private double balance;
 
 	public void setHolderName(String name) {
 		holderName = name;
@@ -29,6 +43,7 @@ public class BankAccount {
 		if(accountNum == accNum && password == pass) {
 			System.out.println("Your name is " + holderName);
 			System.out.println("Your balance is " + balance);
+			System.out.println("Your account number is " + accountNum);
 
 		} else {
 			System.out.println("Something doesn't match");
@@ -44,7 +59,14 @@ public class BankAccount {
 		}
 	}
 
-	public void withdraw(int amount, int accNum, int pin) {}
+	public void withdraw(int amount, int accNum, int inputtedPin) {
+		if (accountNum == accNum && inputtedPin == pin ) {
+			balance -= amount;
+			System.out.println("Successfully withdrawn. Your balance is $" + balance);
+		} else {
+			System.out.println("Your account number or pin doesn't match");
+		}
+	}
 
 	public static void main(String[] args) {
 		BankAccount bofa = new BankAccount();
@@ -55,10 +77,12 @@ public class BankAccount {
 		System.out.println("Password is " + password);
 
 		bofa.setPin(1234);
-		bofa.setAccountNum(1);
+		bofa.setAccountNum(123456789);
 		bofa.setBalance(0);
 
-		bofa.deposit(420000, 1);
-		bofa.printInfo(1, "abc123");
+		bofa.deposit(420000, 123456789);
+		bofa.printInfo(123456789, "abc123");
+		bofa.withdraw(12090, 123456789, 1234);
+		bofa.withdraw(12090, 123456789, 12324); //wrong pin
 	}
 }
