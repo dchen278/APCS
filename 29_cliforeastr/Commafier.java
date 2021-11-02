1ba925f54e2925b0b53d18bbf6645597c6de4b97
dchen278 <reindeer914@gmail.com>
2021-11-02 11:59:16 -0400

    29 cliforeastr

---
+++ 29_cliforeastr/Commafier.java
@@ -0,0 +1,38 @@
+public class Commafier {
+    public static void main(String[] args) {
+        // System.out.println("Recursive version of commafy");
+        // System.out.println(commafyR(1));
+        // System.out.println(commafyR(10));
+        // System.out.println(commafyR(101));
+        // System.out.println(commafyR(1000));
+        // System.out.println(commafyR(12345));
+
+        // System.out.println("\nIterative version of commafy");
+        // System.out.println(commafyF(1));
+        // System.out.println(commafyF(10));
+        // System.out.println(commafyF(101));
+        // System.out.println(commafyF(1000));
+        // System.out.println(commafyF(12345));
+
+        for(String arg : args) {
+            System.out.println(commafyF(Integer.parseInt(arg)));
+        }
+    }
+
+    public static String commafyR(int n) {
+        String number = Integer.toString(n);
+        if (number.length() <= 3) {
+            return number;
+        } else {
+            return commafyR(Integer.parseInt(number.substring(0, number.length() - 3))) + "," + number.substring(number.length() - 3);
+        }
+    }
+
+    public static String commafyF(int n) {
+        String number = Integer.toString(n);
+        for (int i = number.length() - 3; i > 0; i -= 3) {
+            number = number.substring(0, i) + "," + number.substring(i);
+        }
+        return number;
+    }
+}
--- cb/string-1/extraEnd.java
+++ cb/string-1/extraEnd.java
@@ -1,6 +1,6 @@
 public class extraEnd {
   public String extraEnd(String str) {
-  String lastChars = str.substring(str.length() - 2, str.length());
-  return lastChars+lastChars+lastChars;
-}
+    String lastChars = str.substring(str.length() - 2, str.length());
+    return lastChars + lastChars + lastChars;
+  }
 }
