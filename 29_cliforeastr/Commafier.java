fc74c71ca45817cf4d088ca9080f4d85fb9d5f80
dchen278 <reindeer914@gmail.com>
2021-11-03 23:50:42 -0400

    oink

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,13 +1,19 @@
+
 /*
 Downtown Leopard Rocks: David Chen, Lea Kwok, Raven Tang
 APCS
 HW29 -- Which Way Do You Roll? - commafying an integer iteratively and recursively
 11-02-2021
 DISCO
- - There are a variety of wrapper classes for primitive types, such as Integer, Double, etc.
- - The enhanced for loop is a good way to iterate over a collection.
+ - There are a variety of wrapper classes for primitive types, such as Integer, Double, etc. They have their own methods, such as parseInt.
+ - The enhanced for loop is a good way to iterate over a collection. It doesn't require you to know how many items are in the collection or reference array length.
+ - In our summer prework, we used Scanner as a way to take keyboard input. Scanner requires individual input of each piece of information after being prompted. However, in this assignment, iterating through String[] args allows all of the input to be given in the same method call. The two are also different because one actually calls the main method and the other reads file input.
+ - In our for loop, if number's length is less than 3, the loop gets skipped because i > 0 is not fulfilled. So, the condition part of a for loop can be used to restrict iterations for certain cases.
+ - You can increment by a value other than 1. For example, 
+	i -= 3
 QCC
- -N/A
+ - Is there a way to take in an integer input and output a string without using methods like parseInt or toString?
+ - If we were to consider negative inputs, we would use a boolean to record if the input is negative or not, convert the absolute value of the input to a string, and concatenate the negative sign at the end before returning according to the boolean value.
 */
 
 public class Commafier {
---
+++ 31_oink/Pig.java
@@ -0,0 +1,111 @@
+/*
+Downtown Leopard Rocks: David Chen, Lea Kwok, Raven Tang
+APCS
+HW31: Otnay Ybay ethay Airhay Onway Ymay Inneechay Inchay Inchay
+11-03-2021
+Time Spent: 20 minutes
+DISCO
+  - The final keyword is used to indicated that a method or variable cannot be changed after
+    it has been declared.
+  - We can also use indexOf to find the index of a substring in a string.  
+QCC
+ -N/A
+*/
+
+public class Pig {
+    // Q: How does this initialization make your life easier?
+    private static final String VOWELS = "aeiou";
+
+    /*
+     * ===================================== boolean hasA(String,String) -- checks
+     * for a letter in a String pre: w != null, letter.length() == 1 post:
+     * hasA("cat", "a") -> true hasA("cat", "p") -> false
+     * =====================================
+     */
+    public static boolean hasA(String w, String letter) {
+        return w.indexOf(letter) != -1;
+    }// end hasA()
+
+    /*
+     * ===================================== boolean isAVowel(String) -- tells
+     * whether a letter is a vowel precondition: letter.length() == 1
+     * =====================================
+     */
+    public static boolean isAVowel(String letter) {
+        /* YOUR IMPLEMENTATION HERE */
+        return hasA(VOWELS, letter);
+    }
+
+    /*
+     * ===================================== int countVowels(String) -- counts
+     * vowels in a String pre: w != null post: countVowels("meatball") -> 3
+     * =====================================
+     */
+    public static int countVowels(String w) {
+        int count = 0;
+        if (w != null) {
+            for (int i = 0; i < w.length(); i++) {
+                if (isAVowel(w.substring(i, i + 1))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /*
+     * ===================================== boolean hasAVowel(String) -- tells
+     * whether a String has a vowel pre: w != null post: hasAVowel("cat") -> true
+     * hasAVowel("zzz") -> false =====================================
+     */
+    public static boolean hasAVowel(String w) {
+        if (w != null) {
+            for (int i = 0; i < w.length(); i++) {
+                if (isAVowel(w.substring(i, i + 1))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /*
+     * ===================================== String allVowels(String) -- returns
+     * vowels in a String pre: w != null post: allVowels("meatball") -> "eaa"
+     * =====================================
+     */
+    public static String allVowels(String w) {
+        /* YOUR IMPLEMENTATION HERE */
+        String result = "";
+        if (w != null) {
+            for (int i = 0; i < w.length(); i++) {
+                String letter = w.substring(i, i + 1);
+                if (isAVowel(letter)) {
+                    result += letter;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /* YOUR TEST CALLS HERE */
+        System.out.println("Does cat contain the letter a? " + hasA("cat", "a"));
+        System.out.println("Does cat contain the letter p? " + hasA("cat", "p"));
+
+        System.out.println("Is a vowel? " + isAVowel("a"));
+        System.out.println("Is b vowel? " + isAVowel("b"));
+    
+        System.out.println("How many vowels in cat? " + countVowels("cat"));
+        System.out.println("How many vowels in zzz? " + countVowels("zzz"));
+
+        System.out.println("Does cat have a vowel? " + hasAVowel("cat"));
+        System.out.println("Does zzz have a vowel? " + hasAVowel("zzz"));
+
+        System.out.println("All vowels in cat? " + allVowels("cat"));
+        System.out.println("All vowels in spaghetti? " + allVowels("spaghetti"));
+        
+
+    }// end main()
+
+}// end class Pig
