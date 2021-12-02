8feac5058f072f48f32f20eb90e232e4e809429d
dchen278 <reindeer914@gmail.com>
2021-12-01 20:45:48 -0500

    update disco and qcc

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -1,3 +1,18 @@
+/*
+Team Incredibly Cohesive (David Chen, Jaylen Zeng, Orion Roven)
+APCS pd7
+HW41 -- Be Rational
+12/1/21
+time elapsed: 0.3 hours
+DISCO:
+When we say r.multiply(s), in the multiply method, this refers to r and the parameter num refers to s
+If we set rational r to x/0, then r defaults to the word Infinity
+To fix this, in the divide and multiply methods we added a if statement that checks if the denominator is 0, and if it is, it returns from the method
+QCC:
+I thought that there were math errors that happen in java when a number is divided by 0
+Why in this case do the numbers divided by 0 become Infinity
+*/
+
 public class Rational {
     public int denominator;
     public int numerator;
@@ -18,11 +33,11 @@ public class Rational {
         System.out.println(t.floatValue() + " expected: 0.0");
 
         System.out.println("----Test multiply()----");
-        Rational g = new Rational(2,13);
-        Rational h = new Rational(3,17);
+        Rational g = new Rational(2, 13);
+        Rational h = new Rational(3, 17);
         g.multiply(h);
         System.out.println(g + " expected: 6/221");
-        
+
         Rational u = new Rational(1, 2);
         Rational v = new Rational(1, 3);
         u.multiply(v);
