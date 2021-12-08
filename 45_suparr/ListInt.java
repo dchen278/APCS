fcf68a6b168369a36699a1b072b9711893f05d8a
dchen278 <reindeer914@gmail.com>
2021-12-08 18:42:43 -0500

    update interface

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -1,9 +1,6 @@
 public interface ListInt {
-    public int get(int num);
     public void add(int num);
     public void add(int num, int index);
-    public int set(int index, int newVal);
     public int size();
     public void remove();
-    public void remove(int index);
 }
