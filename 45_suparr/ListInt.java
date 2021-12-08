4a27a21b4c1a96e5b68246b54d074f63e2602ec0
dchen278 <reindeer914@gmail.com>
2021-12-08 18:43:26 -0500

    add param

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -2,5 +2,5 @@ public interface ListInt {
     public void add(int num);
     public void add(int num, int index);
     public int size();
-    public void remove();
+    public void remove(int index);
 }
