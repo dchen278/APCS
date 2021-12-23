8f9ba8c9ace1ec7488a7822ba6ed476cc368fcd8
dchen278 <dchen30@stuy.edu>
2021-12-23 00:46:08 -0500

    + test cases

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -7,6 +7,19 @@ public class Driver {
       System.out.println(stillDavid.compareTo(jaylen)); //should return 0;
       System.out.println(jaylen.compareTo(orion));
       System.out.println(orion.compareTo(stillDavid));
-      System.out.println(stillDavid.compareTo(i));
+      // System.out.println(stillDavid.compareTo(i));
+
+      System.out.println("Testing .equals \n");
+      Rational timmy = new Rational(2,4);
+      Rational tommy = new Rational(4,8);
+      Rational tammy = new Rational(5,8);
+      String tim = new String("Imposter");
+      System.out.println(timmy.equals(tommy)); // true
+      System.out.println(tommy.equals(timmy));// true
+      System.out.println(timmy.equals(tammy));// false
+      System.out.println(tammy.equals(tommy)); // false
+      // System.out.println(timmy.equals(tim)); // error
+      System.out.println(tim.equals(tommy)); // false
+      System.out.println(tim.equals(null)); // false
     }
   }
