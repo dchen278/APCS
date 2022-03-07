d9453742cdddff4e8326ea702b5e15b19beccb67
dchen278 <dchen30@stuy.edu>
2022-03-07 14:06:09 -0500

    add 71

---
+++ 71_unmask/Mysterion.java
@@ -0,0 +1,32 @@
+public class Mysterion {
+    public static void main(String[] args) {
+        System.out.println(mysterion(new int[] {7, 1, 5, 12, 3}, 0, 4, 2));
+    }
+
+    public static int[] mysterion(int[] arr, int a, int b, int c) {
+        int v = arr[c];
+        int temp = arr[c];
+        arr[c] = arr[b];
+        arr[b] = temp;
+
+        int s = a;
+
+        for (int i = a; i < b - 1; i++) {
+            if (arr[i] < v) {
+                temp = arr[s];
+                arr[s] = arr[i];
+                arr[i] = temp;
+                s++;
+            }
+        }
+        temp = arr[b];
+        arr[b] = arr[s];
+        arr[s] = temp;
+
+        return arr;
+    }
+
+    public static String printArr(int[] arr) {
+        
+    }
+}
