234d6f79f0b2f1665e4c9e0d5f59e8a8b35589db
dchen278 <dchen30@stuy.edu>
2022-04-05 22:12:40 -0400

    add cereal

--- cobo/data/DataStudentCode/Welcome03_List.java
+++ cobo/data/DataStudentCode/Welcome03_List.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
-             "station/station_id", "station/state",
-             "station/latitude", "station/longitude");
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
+            "station/station_id", "station/state",
+            "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
-      
+
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
@@ -23,5 +23,23 @@ public class Welcome03_List {
             System.out.println("  " + ws.getId() + ": " + ws.getName());
          }
       }
+
+      double southernMostWS = allstns.get(0).getLatitude();
+      String name = allstns.get(0).getName();
+
+      for (WeatherStation ws : allstns) {
+         double lat = ws.getLatitude();
+
+         if (lat > 0 && lat <= southernMostWS) {
+            southernMostWS = lat;
+            name = ws.getName();
+         } else if (lat < 0 && lat <= southernMostWS) {
+            southernMostWS = lat;
+            name = ws.getName();
+         }
+
+         System.out.println("The southern most weeather station is " + name);
+      }
+
    }
 }
